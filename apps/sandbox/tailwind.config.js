d2373196c88038a05aa739c6af945c7eeb695db7
Ryan Maloney <rsmaloney@gmail.com>
2022-07-08 18:34:42 -0600

    added tailwind present file overriding colors

--- apps/sandbox/tailwind.config.js
+++ apps/sandbox/tailwind.config.js
@@ -2,6 +2,7 @@ const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
 
 module.exports = {
+  presets: [require('../../tailwind.preset')],
   content: [
     join(
       __dirname,
